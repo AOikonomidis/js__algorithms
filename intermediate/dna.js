c67115a8e711641f4efe66bd35fce66dc8d41747
Andrew Oikonomidis <andyreneyload@gmail.com>
2017-08-26 21:47:39 +0300

    DNA Pairing

---
+++ intermediate/dna.js
@@ -0,0 +1,56 @@
+/*
+
+  @author: Andrew Oikonomidis
+
+  developed during freeCodeCamp's Front End Development Certification
+
+  -------------------------------------------------------------------
+
+  DNA Pairing
+
+  instructions:
+
+  The DNA strand is missing the pairing element. 
+  Take each character, get its pair, and return 
+  the results as a 2d array.
+
+  Base pairs are a pair of AT and CG. 
+  Match the missing element to the provided character.
+
+  Return the provided character as the first element in each array.
+
+*/
+
+function pairElement(str) {
+  
+  let finalArr = [];
+  
+  let DNA = s => {
+    switch(s) {
+      case 'A':
+        finalArr.push(['A', 'T']);
+        break;
+      case 'T':
+        finalArr.push(['T', 'A']);
+        break;
+      case 'G':
+        finalArr.push(['G', 'C']);
+        break;
+      case 'C':
+        finalArr.push(['C', 'G']);
+        break;
+    }
+  };
+  
+  for(let i=0; i<str.length; i++) {   
+    DNA(str[i]);    
+  }
+
+  return finalArr;
+}
+
+// some testing
+console.log(pairElement("GCG")); // should return [["G", "C"], ["C","G"],["G", "C"]].
+console.log(pairElement("ATCGA")); // should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
+console.log(pairElement("TTGAG")); // should return [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]].
+console.log(pairElement("CTCTA")); // should return [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]].
