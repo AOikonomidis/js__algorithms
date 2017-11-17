928035c4fb83aa0f4a65f47e05539c468ebd6fa2
Andrew Oikonomidis <andyreneyload@gmail.com>
2017-11-17 20:44:04 +0200

    Mutations

--- basic/mutation.js
+++ basic/mutation.js
@@ -17,10 +17,10 @@
 */
 
 function mutation(arr) {
-  var str1 = arr[0].toLowerCase();
-  var str2 = arr[1].toLowerCase();
+  let str1 = arr[0].toLowerCase();
+  let str2 = arr[1].toLowerCase();
   
-  for (var i=0; i<str2.length; i++) {
+  for (let i=0; i<str2.length; i++) {
     if (str1.indexOf(str2[i]) < 0) {
       return false;
     }
